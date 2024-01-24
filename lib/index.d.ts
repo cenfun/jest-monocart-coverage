63be33b056a9800142f0d6ca29dc75ef0d77364b
cenfun <cenfun@gmail.com>
2024-01-24 14:50:15 +0800

    fix types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,25 @@
-export * from "monocart-coverage-reports"
+import type {
+    CoverageReportOptions,
+    ReportDescription,
+    V8CoverageEntry,
+    Watermarks,
+    CoverageResults,
+    CoverageSummary,
+    MetricsSummary,
+    CoverageFile,
+    CoverageRange,
+    AddedResults
+} from "monocart-coverage-reports";
+
+export type {
+    CoverageReportOptions,
+    ReportDescription,
+    V8CoverageEntry,
+    Watermarks,
+    CoverageResults,
+    CoverageSummary,
+    MetricsSummary,
+    CoverageFile,
+    CoverageRange,
+    AddedResults
+}
